e0eb4700772e8751083850abdfa3c9f907622915
seongjin571 <gkstjdwls11@naver.com>
2019-02-05 20:52:36 +0900

    자바 연습

---
+++ src/forwhile.java
@@ -0,0 +1,22 @@
+public class forwhile {
+    public  static void main(String[] args){
+        int a=0;
+        while(a<10){
+            int b=0;
+            while(b<=a) {
+                System.out.print("*");
+                b++;
+            }
+            System.out.println();
+            a++;
+        }
+
+//            for(int i=0; i<=10; i++) {
+//                for(int j=0; j<=i; j++)
+//                    System.out.print("*");
+//                System.out.println();
+//            }
+        }
+    }
+
+
