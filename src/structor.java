31b3fbd6245d7d05133ef0bcd3d2729149f3c4a3
seongjin571 <gkstjdwls11@naver.com>
2019-02-10 16:25:03 +0900

    practice

--- src/structor.java
+++ src/structor.java
@@ -3,7 +3,6 @@ class car{
     int wheel;
     String size;
     car(){
-        System.out.println("생성자");
         color="white";
         wheel=4;
         size="suv";
@@ -16,12 +15,22 @@ class car{
         this.wheel=wheel;
         this.size=size;
     }
+    car(String[] a){
+        this();
+        for(int i=0; i<a.length; i++) {
+            System.out.printf(a[i]+" ");
+        }
+        System.out.println();
+
+    }
 }
 public class structor {
     public static void main(String[]args){
         car p1= new car();
         car p2= new car(8);
         car p3= new car("black",12,"compact");
+        String[] arr={"하","히","후","해","호"};
+        car p4=new car(arr);
         System.out.println(p1.color+" "+p1.wheel+" "+p1.size);
         System.out.println(p2.color+" "+p2.wheel+" "+p2.size);
         System.out.println(p3.color+" "+p3.wheel+" "+p3.size);
