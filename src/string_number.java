12612bb043cd15c2985b7d559811bd1ad484ee88
seongjin571 <gkstjdwls11@naver.com>
2019-02-14 20:40:18 +0900

    practice

--- src/string_number.java
+++ src/string_number.java
@@ -4,20 +4,18 @@ public class string_number {
         Scanner number = new Scanner(System.in);
         String number_input = number.nextLine();
         String new_string="";
-        int count=1;
 
-        for (int i = 0; i < number_input.length()-1; i++){
-                if(number_input.charAt(i)==number_input.charAt(i+1))
+        for (int i = 0; i < number_input.length(); i++){
+            int count=1;
+            for(int j=i; j<number_input.length()-1; j++) {
+                if (number_input.charAt(j) == number_input.charAt(j + 1))
                     count++;
-                else if(number_input.charAt(i)!=number_input.charAt(i+1) ){
-                   new_string+=number_input.charAt(i);
-                   new_string+=count;
-                   count=1;
-                }
-//                else if(number_input.charAt(i+1)==null){
-//                    new_string+=number_input.charAt(i);
-//                    new_string+=count;
-//                }
+                else
+                    break;
+            }
+            new_string+=number_input.charAt(i);
+            new_string+=count;
+            i+=count-1;
         }
 
         System.out.println(new_string);
